0c0aa51b3a1d5f3a62d45ebcf0873f59b246a76b
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-03 13:41:35 -0500

    add test for middle() to check original array isn't modified

--- middle.js
+++ middle.js
@@ -33,17 +33,20 @@ const middle = function(array) {
   }
 };
 
-// Test 1: empty/1 elem/2 elem arrays should return emtpy array
+console.log(`Test 1: empty/1 elem/2 elem arrays should return emtpy array`);
 assertArraysEqual(middle([]), []);
 assertArraysEqual(middle([1]), []);
 assertArraysEqual(middle([1, 2]), []);
-// Test 2: arrays with odd number of elements should return one middle element
+console.log(`Test 2: arrays with odd number of elements should return one middle element`);
 assertArraysEqual(middle([1, 2, 3]), [2]);
 assertArraysEqual(middle(['my', 'name', 'is', 'mitchell', 'pizzacalla']), ['is']);
-// Test 3: arrays with even number of elements should return two middle elements
+console.log(`Test 3: arrays with even number of elements should return two middle elements`);
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
 assertArraysEqual(middle(['will', 'this', 'work?', 'lets', 'find', 'out!']), ['work?', 'lets']);
-
+console.log(`Test 4: check original array is not modified`);
+const testArray = [1, 2, 3, 4]
+middle(testArray);
+assertArraysEqual(testArray, [1, 2, 3, 4]);
 
 
 
