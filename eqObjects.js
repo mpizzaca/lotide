46002b2a3553a58000f22b2211f2bc9b8c25c65f
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-05 18:52:38 -0500

    upgrade eqObjects() to recursively check nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -20,22 +20,25 @@ const eqArrays = function(arrayOne, arrayTwo) {
 };
 
 const eqObjects = function(object1, object2) {
-  const objOneKeys = Object.keys(object1).sort();
-  const objTwoKeys = Object.keys(object2).sort();
-
   // Check key/value count is the same
-  if (objOneKeys.length !== objTwoKeys.length) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
 
   // Check values are the same
   for (let key in object1) {
-    if (object1[key] instanceof Array && object2[key] instanceof Array) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
-    }
-    else if (object1[key] !== object2[key]) {
+    } else if (object1[key] instanceof Object &&
+               object2[key] instanceof Object &&
+               !Array.isArray(object1[key])   &&
+               !Array.isArray(object2[key])) {
+      if(!eqObjects(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
       return false;
     }
   }
@@ -58,3 +61,9 @@ assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: '1', d: ['2', 3, 4] };
 assertEqual(eqObjects(cd, cd2), false);
+
+// Recursive Tests
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { y: 0, z: 1 }, b: 3 }), false); // => false
