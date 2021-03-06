b8e28aa4ca49fec399ed5f242d5be3bf20812229
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-05 19:05:31 -0500

    upgrade eqArrays() to recursively check nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -11,7 +11,12 @@ const eqArrays = function(arrayOne, arrayTwo) {
     return false;
   } else {
     for (let i = 0; i < arrayOne.length; i++) {
-      if (arrayOne[i] !== arrayTwo[i]) {
+      if (Array.isArray(arrayOne[i]) && Array.isArray(arrayTwo[i])) {
+        if (!eqArrays(arrayOne[i], arrayTwo[i])) {
+          return false;
+        }
+      }
+      else if (arrayOne[i] !== arrayTwo[i]) {
         return false;
       }
     }
@@ -24,3 +29,8 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays([1, 2, 3], ['1', '2', '3']), false);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+
+// Recursive Tests
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
