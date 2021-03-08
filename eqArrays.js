f0d5c3842c1ce072b8eb5007afa33e1b3f5b8bae
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-08 13:46:14 -0500

    eqArrays() tests

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arrayOne, arrayTwo) {
   if (arrayOne.length !== arrayTwo.length) {
     return false;
@@ -24,13 +16,4 @@ const eqArrays = function(arrayOne, arrayTwo) {
   }
 };
 
-// Tests
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays([1, 2, 3], ['1', '2', '3']), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
-
-// Recursive Tests
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+module.exports = eqArrays;
