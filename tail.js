5f43ab1ea2981f6e8f8ea4d6843d77cde1df7f4a
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-08 12:43:11 -0500

    tests for tail()

--- tail.js
+++ tail.js
@@ -1,31 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   return array.slice(1);
 };
 
-// Test Case 1: Check returned array elements
-const result = tail([1, 2, 3]);
-assertEqual(result.length, 2);
-assertEqual(result[0], 2);
-assertEqual(result[1], 3);
-
-// Test Case 2: Check original array is unmodified
-const words = ['Light', 'House', 'Labs'];
-tail(words);
-assertEqual(words.length, 3);
-
-// Test Case 3: Check we get an empty array if we pass an array with 1 element
-const word = ['Hi'];
-const empty = tail(word);
-assertEqual(empty.length, 0);
-
-// Test Case 4: Check we get an empty array if we pass in an empty array
-const emptyTwo = tail([]);
-assertEqual(emptyTwo.length, 0);
+module.exports = tail;
